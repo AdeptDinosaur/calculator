c86ed35cedf01624a44e7b58355f7ecf25aa032f
AdeptDinosaur <126434367+AdeptDinosaur@users.noreply.github.com>
2023-04-12 13:28:16 -0400

    Add basic functionality with 2 numbers

--- scripts.js
+++ scripts.js
@@ -20,13 +20,13 @@ let num2 = 0;
 
 function operate(operator, num1, num2) {
     if(operator === "+"){
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator === "-"){
-        subtract(num1, num2);
+        return subtract(num1, num2);
     } else if (operator === "*"){
-        multiply(num1, num2);
+        return multiply(num1, num2);
     } else if (operator === "/"){
-        divide(num1, num2);
+        return divide(num1, num2);
     }
 }
 const display = document.getElementById("display");
@@ -34,60 +34,117 @@ let currentValue = "";
 
 const zero = document.getElementById("zero");
 zero.addEventListener("click", () => {
-    currentValue += "0"
+    currentValue += "0";
     display.innerHTML = currentValue;
 })
 
 const one = document.getElementById("one");
 one.addEventListener("click", () => {
-    currentValue += "1"
+    currentValue += "1";
     display.innerHTML = currentValue;
 })
 
 const two = document.getElementById("two");
 two.addEventListener("click", () => {
-    currentValue += "2"
+    currentValue += "2";
     display.innerHTML = currentValue;
 })
 
 const three = document.getElementById("three");
 three.addEventListener("click", () => {
-    currentValue += "3"
+    currentValue += "3";
     display.innerHTML = currentValue;
 })
 
 const four = document.getElementById("four");
 four.addEventListener("click", () => {
-    currentValue += "4"
+    currentValue += "4";
     display.innerHTML = currentValue;
 })
 
 const five = document.getElementById("five");
 five.addEventListener("click", () => {
-    currentValue += "5"
+    currentValue += "5";
     display.innerHTML = currentValue;
 })
 
 const six = document.getElementById("six");
 six.addEventListener("click", () => {
-    currentValue += "6"
+    currentValue += "6";
     display.innerHTML = currentValue;
 })
 
 const seven = document.getElementById("seven");
 seven.addEventListener("click", () => {
-    currentValue += "7"
+    currentValue += "7";
     display.innerHTML = currentValue;
 })
 
 const eight = document.getElementById("eight");
 eight.addEventListener("click", () => {
-    currentValue += "8"
+    currentValue += "8";
     display.innerHTML = currentValue;
 })
 
 const nine = document.getElementById("nine");
 nine.addEventListener("click", () => {
-    currentValue += "9"
+    currentValue += "9";
     display.innerHTML = currentValue;
 })
+
+const del = document.getElementById("delete")
+del.addEventListener("click", () => {
+    currentValue = currentValue.substring(0, currentValue.length-1);
+    display.innerHTML = currentValue;
+})
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+    currentValue = "";
+    display.innerHTML = currentValue;
+})
+
+const subtraction = document.getElementById("subtract");
+subtraction.addEventListener("click", () => {
+    num1 = currentValue;
+    currentValue = "";
+    operator = "-";
+    display.innerHTML = "";
+    
+})
+
+const multiplication = document.getElementById("multiply");
+multiplication.addEventListener("click", () => {
+    num1 = currentValue;
+    currentValue = "";
+    operator = "*";
+    display.innerHTML = "";
+    
+})
+
+const division = document.getElementById("divide");
+division.addEventListener("click", () => {
+    num1 = currentValue;
+    currentValue = "";
+    operator = "/";
+    display.innerHTML = "";
+    
+})
+
+const addition = document.getElementById("add");
+addition.addEventListener("click", () => {
+    num1 = currentValue;
+    currentValue = "";
+    operator = "+";
+    display.innerHTML = "";
+    
+})
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", () => {
+    num2 = currentValue;
+    num1 = Number(num1);
+    num2 = Number(num2);
+    display.innerHTML = operate(operator, num1, num2);
+})
+
